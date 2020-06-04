3f5d01ceded91332a2bdb6cc4e59ed0f494cdd69
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-06-04 09:32:39 +0800

    Run 'terraform fmt -recursive'

--- main.tf
+++ main.tf
@@ -33,17 +33,17 @@ resource "google_project_service" "redis_api" {
 }
 
 resource "google_redis_instance" "redis_store" {
-  name               = local.memory_store_name
-  memory_size_gb     = var.memory_size_gb
-  display_name       = local.memory_store_display_name
-  redis_version      = var.redis_version
-  tier               = var.service_tier
-  authorized_network = var.vpc_network
-  region             = local.region
-  location_id        = local.primary_zone
+  name                    = local.memory_store_name
+  memory_size_gb          = var.memory_size_gb
+  display_name            = local.memory_store_display_name
+  redis_version           = var.redis_version
+  tier                    = var.service_tier
+  authorized_network      = var.vpc_network
+  region                  = local.region
+  location_id             = local.primary_zone
   alternative_location_id = var.service_tier != "STANDARD_HA" ? null : local.alternate_zone
-  reserved_ip_range  = var.ip_cidr_range
-  depends_on         = [google_project_service.redis_api]
+  reserved_ip_range       = var.ip_cidr_range
+  depends_on              = [google_project_service.redis_api]
   timeouts {
     create = var.redis_timeout
     update = var.redis_timeout
